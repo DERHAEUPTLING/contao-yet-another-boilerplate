1e905d63553c633c208ecc95764a292b4d0b4a79
Martin Schwenzer <mail@derhaeuptling.com>
2014-08-12 12:34:03 +0200

    changed box-sizing to inherit from html

--- app/js/entry.js
+++ app/js/entry.js
@@ -6,6 +6,7 @@
  * static includes
  *
  */
+
 var jQuery = require("jquery");
 
 var basic = require("./chunk/basic.js");
--- build/Gruntfile.js
+++ build/Gruntfile.js
@@ -171,45 +171,6 @@ grunt.initConfig({
    * WEBPACK
    */
   webpack: {
-      dev: {
-        // webpack options
-        entry: "./js/entry.js",
-        output: {
-            path: "../files/dist/js/",
-            publicPath: "files/dist/js/",
-            // filename: "[name].bundle.js",
-            // namedChunkFilename: "[name].bundle.js"
-        },
-        externals: {
-            // require("jquery") is external and available
-            //  on the global var jQuery
-            "jquery": "jQuery"
-        },
-
-        stats: {
-            // Configure the console output
-            colors: true,
-            modules: true,
-            reasons: false
-        },
-        // stats: false disables the stats output
-
-        storeStatsTo: "xyz", // writes the status to a variable named xyz
-        // you may use it later in grunt i.e. <%= xyz.hash %>
-
-        progress: false, // Don't show progress
-        // Defaults to true
-
-        failOnError: true, // don't report error to grunt if webpack find errors
-        // Use this if webpack errors are tolerable and grunt should continue
-
-        //watch: true, // use webpacks watcher
-        // You need to keep the grunt process alive
-
-        //keepalive: true, // don't finish the grunt task
-        // Use this in combination with the watch option
-
-      },
 
       dev: {
 
