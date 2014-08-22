9f9b57ff2c2aec01be81d35a4fdfb4853d3230e7
Martin Schwenzer <mail@derhaeuptling.com>
2014-08-22 15:01:44 +0200

    add imagesLoaded.js and JS cleanup

--- app/js/entry.js
+++ app/js/entry.js
@@ -7,15 +7,16 @@
  *
  */
 
-var jQuery = require("jquery");
+var jQuery 		= require("jquery");
 
-var basic = require("./chunk/basic.js");
-var autogrow = require("./chunk/autogrow.js");
+var basic 		= require("./chunk/basic.js");
+var autogrow 	= require("./chunk/autogrow.js");
 
 
 
-var offcanvas = require('./chunk/offcanvas.js');
+var offcanvas  	= require('./chunk/offcanvas.js');
 var scrollTimer = require('./chunk/scrollTimer.js');
+var imagesLoaded= require('./chunk/imagesloaded.js');
 
 
 /**
@@ -32,7 +33,27 @@ if (Modernizr.touch) {
 
 
 
+// /**
+//  * imagesLoaded
+//  * http://imagesloaded.desandro.com/
+//  */
+// var imagesLoaded = require("./bower/imagesloaded.pkgd.min.js");
 
+// // triggered after each item is loaded
+// function onProgress( imgLoad, image ) {
+//   // change class if the image is loaded or broken
+//   var $item = $( image.img ).parent();
+//   $item.addClass('is-loaded');
+//   if ( !image.isLoaded ) {
+//     $item.addClass('is-broken');
+//   }
+// }
+
+// $('#container').imagesLoaded()
+// 	// .always( function( instance ) {
+// 	// 	console.log('all images loaded');
+// 	// })
+// 	.progress( onProgress );
 
 
 
