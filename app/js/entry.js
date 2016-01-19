a2151b03e4ab928c285cfadf5b765688d7ff06f4
Martin Schwenzer <mail@derhaeuptling.com>
2016-01-19 17:05:55 +0100

    webpack for gulp

--- app/js/entry.js
+++ app/js/entry.js
@@ -13,11 +13,9 @@ var basic 		= require("./chunk/basic.js");
 var autogrow 	= require("./chunk/autogrow.js");
 
 
-
 var offcanvas  	= require('./chunk/offcanvas.js');
 var scrollTimer = require('./chunk/scrollTimer.js');
 var imagesLoaded= require('./chunk/imagesloaded.js');
-
 
 /**
  * load swipe if touch device
@@ -27,33 +25,10 @@ if (Modernizr.touch) {
 	//console.log("has touch");
 	require.ensure([], function(require) {
         var touchswipe = require('./chunk/touchswipe.js');
-	});
+	}, 'touchswipe');
 }
 
 
-
-
-// /**
-//  * imagesLoaded
-//  * http://imagesloaded.desandro.com/
-//  */
-// var imagesLoaded = require("./bower/imagesloaded.pkgd.min.js");
-
-// // triggered after each item is loaded
-// function onProgress( imgLoad, image ) {
-//   // change class if the image is loaded or broken
-//   var $item = $( image.img ).parent();
-//   $item.addClass('is-loaded');
-//   if ( !image.isLoaded ) {
-//     $item.addClass('is-broken');
-//   }
-// }
-
-// $('#container').imagesLoaded()
-// 	// .always( function( instance ) {
-// 	// 	console.log('all images loaded');
-// 	// })
-// 	.progress( onProgress );
 
 
 
