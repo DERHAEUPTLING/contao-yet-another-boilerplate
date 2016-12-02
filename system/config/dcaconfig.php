7ce3e29ceff7389dd7c6547af906b4f9cbe730d7
Martin Schwenzer <mail@derhaeuptling.com>
2016-12-02 11:16:25 +0100

    prepare all CEs to be unset

--- system/config/dcaconfig.php
+++ system/config/dcaconfig.php
@@ -61,3 +61,65 @@
 
 // Sets the default form field
 // $GLOBALS['TL_DCA']['tl_form_field']['fields']['type']['default'] = 'hidden';
+
+
+
+
+
+/*
+ * remove unwanted CEs
+ */
+
+
+// texts
+
+// unset($GLOBALS['TL_CTE']['texts']['headline']);
+// unset($GLOBALS['TL_CTE']['texts']['text']);
+// unset($GLOBALS['TL_CTE']['texts']['html']);
+// unset($GLOBALS['TL_CTE']['texts']['list']);
+// unset($GLOBALS['TL_CTE']['texts']['table']);
+// unset($GLOBALS['TL_CTE']['texts']['code']);
+// unset($GLOBALS['TL_CTE']['texts']['markdown']);
+
+
+// accordion
+
+// unset($GLOBALS['TL_CTE']['accordion']['accordionSingle']);
+// unset($GLOBALS['TL_CTE']['accordion']['accordionStart']);
+// unset($GLOBALS['TL_CTE']['accordion']['accordionStop']);
+
+
+
+// slider
+
+// unset($GLOBALS['TL_CTE']['slider']['sliderStart']);
+// unset($GLOBALS['TL_CTE']['slider']['sliderStop']);
+
+
+// links
+
+// unset($GLOBALS['TL_CTE']['links']['hyperlink']);
+// unset($GLOBALS['TL_CTE']['links']['toplink']);
+
+
+// media
+
+// unset($GLOBALS['TL_CTE']['media']['image']);
+// unset($GLOBALS['TL_CTE']['media']['gallery']);
+// unset($GLOBALS['TL_CTE']['media']['youtube']);
+// unset($GLOBALS['TL_CTE']['media']['player']);
+
+
+// files
+
+// unset($GLOBALS['TL_CTE']['files']['download']);
+// unset($GLOBALS['TL_CTE']['files']['downloads']);
+
+
+// includes
+
+// unset($GLOBALS['TL_CTE']['includes']['article']);
+// unset($GLOBALS['TL_CTE']['includes']['alias']);
+// unset($GLOBALS['TL_CTE']['includes']['form']);
+// unset($GLOBALS['TL_CTE']['includes']['module']);
+// unset($GLOBALS['TL_CTE']['includes']['teaser']);
