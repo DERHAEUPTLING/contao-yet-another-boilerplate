75fdc73e09e1f7c852924339c590e72ca4d32034
Martin Schwenzer <mail@derhaeuptling.com>
2018-11-05 15:54:55 +0100

    added some config files

---
+++ app/Resources/contao/config/config.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Contao Open Source CMS
+ *
+ * Copyright (c) 2005-2017 Leo Feyer
+ *
+ * @license LGPL-3.0+
+ */
+
+/**
+ * Image crop modes
+ */
+$GLOBALS['TL_CROP'] = array
+(
+	'image_sizes' => array
+	(
+		// will be added dynamically
+	),
+	'relative' => array
+	(
+		'proportional', 'box'
+	),
+	'exact' => array
+	(
+		// 'crop',
+		// 'left_top',    'center_top',    'right_top',
+		// 'left_center', 'center_center', 'right_center',
+		// 'left_bottom', 'center_bottom', 'right_bottom'
+	)
+);
